52f442e8ac039426467583657507a93e2fc5dde0
swapnil_06 <60195863+rishusingh022@users.noreply.github.com>
2022-01-14 13:59:42 +0530

    Time: 38 ms (25.56%), Space: 31.6 MB (15.22%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> prefix(n);
+        prefix[0]=nums[0];
+        for(int i=1;i<n;i++) prefix[i]=prefix[i-1]+nums[i];
+        for(int i=0;i<n;i++){
+            if(prefix[i]-nums[i]==prefix[n-1]-prefix[i]) return i;
+        }
+        return -1;
+    }
+};
