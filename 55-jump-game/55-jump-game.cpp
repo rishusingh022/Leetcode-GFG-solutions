c1fcca25f664774a01d6bac349208d4c1bcd4ba5
swapnil_06 <60195863+rishusingh022@users.noreply.github.com>
2022-01-18 19:40:46 +0530

    Time: 135 ms (25.15%), Space: 48.2 MB (94.58%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,9 +1,30 @@
 class Solution {
 public:
+     bool solution1(vector<int>& nums){
+        //cornor cases
+        int n=nums.size();
+        if(n<=1) return true;
+        if(nums[0]==0) return false;
+        
+        // main code starts
+        int step=nums[0];
+        int maxReach=nums[0];
+        //int jump=1;
+        int i=1;
+        for(i=1;i<n;i++){
+            if(i==n-1) return true;
+            maxReach=max(maxReach,i+nums[i]);
+            step--;
+            if(step==0){
+                if(i>=maxReach) return false;
+                step=maxReach-i;
+                //jump++;
+            }
+        }
+        return false;
+    }
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        //cornor cases
-        
         if(n<=1) return true;
         if(nums[0]==0) return false;
         
