e0f9219a69f8ff69b36d3005bffbb03668def224
swapnil_06 <60195863+rishusingh022@users.noreply.github.com>
2022-01-19 13:42:34 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.cpp
+++ Bubble Sort - GFG/bubble-sort.cpp
@@ -24,11 +24,14 @@ class Solution
     {
         // Your code here  
         for(int i=1;i<n;i++){
-            for(int j=0;j<n-1;j++){
+            int flag=0;
+            for(int j=0;j<n-i;j++){
                 if(arr[j]>arr[j+1]){
                     swap(&arr[j],&arr[j+1]);
+                    flag=1;
                 }
             }
+            if(flag==0) break;
         }
     }
 };
