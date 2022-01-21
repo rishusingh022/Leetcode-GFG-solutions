7d7b324425a5204bb87f62a1e0d243df3943d5be
swapnil_06 <60195863+rishusingh022@users.noreply.github.com>
2022-01-21 22:20:53 +0530

    Time: 4 ms (88.90%), Space: 7.9 MB (73.22%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans;
-        for(int i=0;i<=n;i++){
-            int cnt=0;
-            int temp=i;
-            while(temp>0){
-                cnt++;
-                temp=temp&(temp-1);
+        vector<int> dp(n+1);
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                dp[i]=dp[i/2];
             }
-            ans.push_back(cnt);
+            else{
+                dp[i]=1+dp[i/2];
+            }
         }
-        return ans;
+        return dp;
     }
 };
