fd610a29f1522e7df3827a048614069bcc0d26c2
swapnil_06 <60195863+rishusingh022@users.noreply.github.com>
2022-02-05 10:56:09 +0530

    Added solution - LeetHub

---
+++ Move all zeros to the front of the linked list - GFG/move-all-zeros-to-the-front-of-the-linked-list.cpp
@@ -0,0 +1,87 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void push(struct Node **head_ref, int new_data) {
+    struct Node *new_node = new Node(new_data);
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void moveZeroes(struct Node **head);
+
+// function to displaying nodes
+void display(struct Node *head) {
+    while (head != NULL) {
+	    cout << head->data << " ";
+	    head = head->next;
+    }
+    cout <<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int q;
+        struct Node *Head=NULL;
+        for(int i=0;i<n;i++)
+        {
+            cin>>q;
+            push(&Head,q);
+        }
+        moveZeroes(&Head);
+        display(Head);
+
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+/* Linked List Node structure
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+// Move Zeros to the front of the list
+void moveZeroes(struct Node **head)
+{
+    //Your code here
+    Node *temp=*head;
+    while(temp->next!=NULL){
+        if(temp->next->data==0){
+            Node *mvptr=temp->next;
+            temp->next=temp->next->next;
+            mvptr->next=*head;
+            *head=mvptr;
+        }
+        else{
+            temp=temp->next;
+        }
+    }
+}
