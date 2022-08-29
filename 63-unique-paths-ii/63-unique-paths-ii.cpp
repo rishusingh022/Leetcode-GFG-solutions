ff81971c69656835f9b14607c50351c041f29e50
swapnil_06 <60195863+rishusingh022@users.noreply.github.com>
2022-08-29 21:58:56 +0530

    Time: 3 ms (84.83%), Space: 7.8 MB (45.12%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+   int solve(vector<vector<int>>& oG,int i,int j,vector<vector<int>> &dp){
+       if(i==oG.size()-1 && j==oG[0].size()-1){
+            return 1;
+       }
+       if(oG[i][j]==1){
+           return 0;
+       }
+       if(dp[i][j]!=-1){
+           return dp[i][j];
+       }
+       int left=0,right=0;
+       if(i<oG.size()-1){
+           left=solve(oG,i+1,j,dp);
+       }
+       if(j<oG[0].size()-1){
+           right=solve(oG,i,j+1,dp);
+       }
+       return dp[i][j]=left+right;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        if(obstacleGrid[m-1][n-1]==1){
+            return 0;
+        }
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return solve(obstacleGrid,0,0,dp);
+    }
+};
