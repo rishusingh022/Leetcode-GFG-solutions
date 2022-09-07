e692081c6d8d1df796bfc454a3d3819f91aa47e5
swapnil_06 <60195863+rishusingh022@users.noreply.github.com>
2022-09-07 12:52:42 +0530

    Time: 17 ms (65.82%), Space: 9.8 MB (51.90%) - LeetHub

--- 1815-maximum-number-of-groups-getting-fresh-donuts/1815-maximum-number-of-groups-getting-fresh-donuts.cpp
+++ 1815-maximum-number-of-groups-getting-fresh-donuts/1815-maximum-number-of-groups-getting-fresh-donuts.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-int n;
-map<vector<int>,int> m; 
-int bs;
-int solve(vector<int> cnt,int left){
+
+int solve(vector<int> cnt,int left,int bs,int n,map<vector<int>,int> &m){
     if(m.find(cnt)!=m.end()){
         return m[cnt];
     }
@@ -12,7 +10,7 @@ int solve(vector<int> cnt,int left){
     for(int i=1;i<lim;i++){
         if(cnt[i]>0){
             cnt[i]--;
-            temp=max(temp,(left==0)+solve(cnt,(left+bs-i)%lim));
+            temp=max(temp,(left==0)+solve(cnt,(left+bs-i)%lim,bs,n,m));
             cnt[i]++;
         }
     }
@@ -21,20 +19,22 @@ int solve(vector<int> cnt,int left){
 }
 int maxHappyGroups(int batchSize,vector<int>&groups){
     int n=groups.size();
-    int res=0;
+    int yo=0;
+    map<vector<int>,int> m; 
+    int bs;
     vector<int> cnt(batchSize,0);
     for(auto i:groups){
         if(i%batchSize==0){
-            res++;
+            yo++;
         }
         else if(cnt[batchSize-i%batchSize]>=1){
-            res++;
+            yo++;
             cnt[batchSize-i%batchSize]--;
         }
         else{
             cnt[i%batchSize]++;
         }
     }
-    return solve(cnt,0)+res;
+    return solve(cnt,0,bs,n,m)+yo;
 }
 };
