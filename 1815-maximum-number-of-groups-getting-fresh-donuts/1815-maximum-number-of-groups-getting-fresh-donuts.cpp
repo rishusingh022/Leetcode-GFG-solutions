7820e1f7221a3d46d137303578857202793061d8
swapnil_06 <60195863+rishusingh022@users.noreply.github.com>
2022-09-07 12:49:13 +0530

    Time: 20 ms (58.23%), Space: 9.8 MB (56.96%) - LeetHub

--- 1815-maximum-number-of-groups-getting-fresh-donuts/1815-maximum-number-of-groups-getting-fresh-donuts.cpp
+++ 1815-maximum-number-of-groups-getting-fresh-donuts/1815-maximum-number-of-groups-getting-fresh-donuts.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    int n;
-    map<vector<int>,int> m; 
-    int bs;
-    int solve(vector<int> cnt,int left){
+int n;
+map<vector<int>,int> m; 
+int bs;
+int solve(vector<int> cnt,int left){
     if(m.find(cnt)!=m.end()){
-    return m[cnt];
-    }
-    int lim=cnt.size(); int temp=0;
-    for(int i=1;i<lim;i++){
-    if(cnt[i]>=1){//if a no. exists whose mod with batchsize gives remainder 1, then move along
-    cnt[i]--;
-    temp=max(temp,(left==0)+solve(cnt,(left+bs-i)%lim));
-    cnt[i]++;
-    }
-
-        }
-        m[cnt]=temp;
         return m[cnt];
     }
-    int maxHappyGroups(int b, vector<int>& groups) {
-        n=groups.size(); int res=0; vector<int> cnt(b,0);
-        bs=b;
-        for(int i=0;i<n;i++){
-            if(groups[i]%b==0){
-                res++;
-            }
-            else if(cnt[b-groups[i]%b]>=1){
-                res++; cnt[b-groups[i]%b]--;
-            }
-            else{
-                cnt[groups[i]%b]++;
-            }
+    int lim=cnt.size();
+    int temp=0;
+    for(int i=1;i<lim;i++){
+        if(cnt[i]>0){
+            cnt[i]--;
+            temp=max(temp,(left==0)+solve(cnt,(left+bs-i)%lim));
+            cnt[i]++;
         }
-        return solve(cnt,0)+res;
-        
     }
+    m[cnt]=temp;
+    return m[cnt];
+}
+int maxHappyGroups(int batchSize,vector<int>&groups){
+    int n=groups.size();
+    int res=0;
+    vector<int> cnt(batchSize,0);
+    for(auto i:groups){
+        if(i%batchSize==0){
+            res++;
+        }
+        else if(cnt[batchSize-i%batchSize]>=1){
+            res++;
+            cnt[batchSize-i%batchSize]--;
+        }
+        else{
+            cnt[i%batchSize]++;
+        }
+    }
+    return solve(cnt,0)+res;
+}
 };
